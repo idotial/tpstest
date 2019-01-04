8c4f5b15da62b59639af356136b164b16343848a
homeMac <724104612@qq.com>
2019-01-04 14:07:47 +0800

    f

--- src/prepare.js
+++ src/prepare.js
@@ -57,7 +57,10 @@ class RepeatBatchSendCoin {
           gasPrice:'1000000000',
           nonce: this.nonce++,
         },baseprivate)
-        await web3.eth.sendSignedTransaction(txObject.rawTransaction)
+        web3.eth.sendSignedTransaction(txObject.rawTransaction)
+        .once('transactionHash', console.log)
+        .once('confirmation', console.log)
+        .once('error', console.log)
         //"0xf86580843b9aca008303345094b41b3986c377a8f914bf0a6da54b6f7a60610819018081d8a02e06a377269bbfd14e39b4b41caaf199e15ef190cf8f4897bd90e8bc8c2cd485a04e4084014386b6b8c49bb18e3977e0cc58180b8ebe1575e660c3957e4fb636ff"
         // batch.add(web3.eth.sendSignedTransaction.request(txObject.rawTransaction))
     }
