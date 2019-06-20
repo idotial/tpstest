5f3a3884e86317e4899cfcc879507a6c35f63882
homeMac <724104612@qq.com>
2019-06-20 16:29:50 +0800

    t

--- src/prepare.js
+++ src/prepare.js
@@ -57,7 +57,7 @@ class RepeatBatchSendCoin {
           from: addrBase,
           to: address,
           value: '10000',
-          chainId: 90,
+          chainId: '0x5a',
           gas: '210000', //100个地址的话差不多时两百万左右，具体可以测试的时候看下交易的gas used做调整
           gasPrice:'1000000000',
           nonce: this.nonce++,
