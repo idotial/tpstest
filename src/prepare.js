f23fc94ab470612be698a8396c4969a0e6400cf2
homeMac <724104612@qq.com>
2019-06-20 12:02:38 +0800

    t

--- src/prepare.js
+++ src/prepare.js
@@ -3,7 +3,7 @@ var Web3 = require('web3');
 var net = require('net');
 var accounts = require('../config/accounts')
 var nodes = require('../config/nodes')
-var web3 = new Web3(new Web3.providers.IpcProvider(nodes[0].url), net);
+var web3 = new Web3(new Web3.providers.IpcProvider(nodes[0].url, net));
 
 const PowerLimit = 1509715260000000
 const transPerBatch = 1
