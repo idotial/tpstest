1b6fe5465f3558f8885694af194778e0ad3be0c2
homeMac <724104612@qq.com>
2019-06-21 10:30:06 +0800

    t

--- src/task.js
+++ src/task.js
@@ -11,112 +11,107 @@ const PowerLimit = 5515982000000000
 const transPerBatch = 5
 
 class RepeatBatchSendCoin {
-  constructor (){
-    this.availbleAccounts = new Set();
-    this.power = new Map();
-    this.nonce = new Map();
-    this.sended = 0;
-    this.isAvailble = true;
-  }
-
-  async refreshAvailbleAddress() {
-    for(let address of accounts.keys()) {
-      try {
-        let power = execFileSync(`/home/ec2-user/go-etherzero/build/bin/geth`, ['attach', '/home/ec2-user/.etztest/geth.ipc', '--exec',  `eth.getPower("${address}")`]);
-        // let power = await web3.eth.getPower(address);
-        power = Number(power);
-        this.power.set(address, power);
-        console.log(address + ':' + power);
-        if (power < PowerLimit) {
-          this.availbleAccounts.delete(address);
-        } else {
-          this.availbleAccounts.add(address);
-        }
-      } catch (e) {
-        taskLogger.error(e.toString());
-      }
+    constructor() {
+        this.availbleAccounts = new Set();
+        this.power = new Map();
+        this.nonce = new Map();
+        this.sended = 0;
+        this.isAvailble = true;
     }
-    // console.log(this.availbleAccounts);
-  }
 
-  checkNode() {
-    const geth = execFile(`/home/ec2-user/go-etherzero/build/bin/geth`, ['attach', '/home/ec2-user/.etztest/geth.ipc', '--exec',  'txpool.status'], (error, stdout, stderr) => {
-      if (error) {
-        taskError.error(error);
-      } else {
-        try {
-          let data = eval('(' + stdout + ')')
-          if (data.pending + data.queued > 2000) {
-            // if (this.intervalId != null) {
-            //   clearInterval(this.intervalId)
-            //   this.intervalId = null;
-            //   taskLogger.info('task stop');
-            // }
-            if (this.isAvailble) {
-              this.isAvailble = false;
-              taskLogger.info('task stop');
+    async refreshAvailbleAddress() {
+        for (let address of accounts.keys()) {
+            try {
+                let power = execFileSync(`/home/ec2-user/go-etherzero/build/bin/geth`, ['attach', '/home/ec2-user/.etztest/geth.ipc', '--exec', `eth.getPower("${address}")`]);
+                // let power = await web3.eth.getPower(address);
+                power = Number(power);
+                this.power.set(address, power);
+                console.log(address + ':' + power);
+                if (power < PowerLimit) {
+                    this.availbleAccounts.delete(address);
+                } else {
+                    this.availbleAccounts.add(address);
+                }
+            } catch (e) {
+                taskLogger.error(e.toString());
             }
-          } else if (!this.isAvailble) {
-            taskLogger.info('task restart');
-            this.isAvailble = true
-          }
-          console.log(data);
+        }
+        // console.log(this.availbleAccounts);
+    }
+
+    checkNode() {
+        try {
+            let status = execFileSync(`/home/ec2-user/go-etherzero/build/bin/geth`, ['attach', '/home/ec2-user/.etztest/geth.ipc', '--exec', 'txpool.status']);
+            let data = eval('(' + status + ')')
+            if (data.pending + data.queued > 2000) {
+                // if (this.intervalId != null) {
+                //   clearInterval(this.intervalId)
+                //   this.intervalId = null;
+                //   taskLogger.info('task stop');
+                // }
+                if (this.isAvailble) {
+                    this.isAvailble = false;
+                    taskLogger.info('task stop');
+                }
+            } else if (!this.isAvailble) {
+                taskLogger.info('task restart');
+                this.isAvailble = true
+            }
+            console.log(data);
         } catch (e) {
             taskLogger.error(e.toString());
             taskLogger.error(data.toString());
             if (this.isAvailble) {
-              this.isAvailble = false;
-              taskLogger.info('task stop');
+                this.isAvailble = false;
+                taskLogger.info('task stop');
             }
         }
-      }
-    });
-  }
+    }
 
-  async refreshNonce(address) {
-    let nonce = await web3.eth.getTransactionCount(address, 'pending');
-    this.nonce.set(address, nonce);
-    console.log(this.nonce);
-  }
+    async refreshNonce(address) {
+        let nonce = await web3.eth.getTransactionCount(address, 'pending');
+        this.nonce.set(address, nonce);
+        console.log(this.nonce);
+    }
 
-  async sendcoin() {
-    if (this.isAvailble) {
-      let batch = new web3.eth.BatchRequest()
-      for (let address of this.availbleAccounts) {
-        for (let i = 0; i < transPerBatch; i++) {
-          try {
-            let txObject = await web3.eth.accounts.signTransaction({
-              to: '0xAABe8da4AF6CCC2d8DeF6F4e22DcE92B0cc845bd',
-              value: '1',
-              chainId: 90,
-              gas: '210000',
-              gasPrice:'1000000000',
-              nonce: this.nonce.get(address),
-            },accounts.get(address))
-            this.nonce.set(address, this.nonce.get(address)+1),
-            this.sended ++;
-            batch.add(web3.eth.sendSignedTransaction.request(txObject.rawTransaction))
-          } catch (e) {
-            taskLogger.error(e.toString());
-          }
+    async sendcoin() {
+        if (this.isAvailble) {
+            let batch = new web3.eth.BatchRequest()
+            for (let address of this.availbleAccounts) {
+                for (let i = 0; i < transPerBatch; i++) {
+                    try {
+                        let txObject = await web3.eth.accounts.signTransaction({
+                            to: '0xAABe8da4AF6CCC2d8DeF6F4e22DcE92B0cc845bd',
+                            value: '1',
+                            chainId: 90,
+                            gas: '210000',
+                            gasPrice: '1000000000',
+                            nonce: this.nonce.get(address),
+                        }, accounts.get(address))
+                        this.nonce.set(address, this.nonce.get(address) + 1),
+                            this.sended++;
+                        batch.add(web3.eth.sendSignedTransaction.request(txObject.rawTransaction))
+                    } catch (e) {
+                        taskLogger.error(e.toString());
+                    }
+                }
+            }
+            taskLogger.info('sended: ' + this.sended);
+            batch.execute()
         }
-      }
-      taskLogger.info('sended: '+this.sended);
-      batch.execute()
     }
-  }
 
-  async start() {
-    for (let address of accounts.keys()) {
-      await this.refreshNonce(address)
+    async start() {
+        for (let address of accounts.keys()) {
+            await this.refreshNonce(address)
+        }
+        // console.log(this.nonce);
+        await this.refreshAvailbleAddress()
+        // console.log(this.availbleAccounts);
+        setInterval(this.refreshAvailbleAddress.bind(this), 1000)
+        setInterval(this.checkNode.bind(this), 1000)
+        setInterval(this.sendcoin.bind(this), 100)
     }
-    // console.log(this.nonce);
-    await this.refreshAvailbleAddress()
-    // console.log(this.availbleAccounts);
-    setInterval(this.refreshAvailbleAddress.bind(this), 1000)
-    setInterval(this.checkNode.bind(this), 1000)
-    setInterval(this.sendcoin.bind(this), 100)
-  }
 }
 
 var task = new RepeatBatchSendCoin()
