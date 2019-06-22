718b94b3a9d736b5eb0ed20d289fd2aa6d3fc04d
homeMac <724104612@qq.com>
2019-06-22 16:32:50 +0800

    自动生成地址

---
+++ build.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+var Web3 = require('web3');
+var web3 = new Web3();
+
+if (!fs.existsSync('config/accounts.js')) {
+  var data = `var accounts = new Map();
+`
+  for (var i = 0; i < 10; i++) {
+    let account = web3.eth.accounts.create();
+    data += `accounts.set('${account.address}', 'account.privateKey');
+`
+  }
+  data += 'module.exports = accounts;'
+  fs.writeFileSync('config/accounts.js', data);
+}
