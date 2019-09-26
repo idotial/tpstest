14950202393ddf18bb54956c9eda73f71cd83c84
homeMac <724104612@qq.com>
2019-09-26 18:11:28 +0800

    f

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ var web3 = new Web3();
 if (!fs.existsSync('config/accounts.js')) {
   var data = `var accounts = new Map();
 `
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 3; i++) {
     let account = web3.eth.accounts.create();
     data += `accounts.set('${account.address}', '${account.privateKey}');
 `
